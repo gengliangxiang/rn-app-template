dec1ec0a1ad6f698d0bd459ae315add2875dbb1e
耿良祥 <gengliangxiang@gengliangxiangdeMacBook-Pro.local>
2020-05-13 14:06:53 +0800

    init template

---
+++ ios/Pods/Target Support Files/Pods-appRN-appRNTests/Pods-appRN-appRNTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_appRN_appRNTests : NSObject
+@end
+@implementation PodsDummy_Pods_appRN_appRNTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-appRN/Pods-appRN-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_appRN : NSObject
+@end
+@implementation PodsDummy_Pods_appRN
+@end
